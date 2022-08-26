5730330e35118fee87db9f170666c81d1b99372f
Sam Bocharov <semen_bard@mail.ru>
2022-08-26 19:41:06 +0200

    login and register pages basig setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,12 +8,24 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/auth',
+    name: 'Auth',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Auth.vue'),
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+      },
+    ],
   },
 ];
 
